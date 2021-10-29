6b1b7ebb44368e7343be483a2a099e626452fb66
tomgrohnert <tpgrohnert@uc.cl>
2021-10-29 15:20:15 -0300

    Updates

---
+++ precache-manifest.8e839bd4e8867f3e4f8af4a8fbae4f4c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e86cb68c4be4724d8b18272d1fa6bab",
+    "url": "/tarea3/index.html"
+  },
+  {
+    "revision": "01b6abd926b36e14f860",
+    "url": "/tarea3/static/css/2.bf07cc69.chunk.css"
+  },
+  {
+    "revision": "36f158478dda8930dd30",
+    "url": "/tarea3/static/css/main.cd73dcd2.chunk.css"
+  },
+  {
+    "revision": "01b6abd926b36e14f860",
+    "url": "/tarea3/static/js/2.7ceb1bb6.chunk.js"
+  },
+  {
+    "revision": "ca8caf20782050028eddd20cf0cc1ed7",
+    "url": "/tarea3/static/js/2.7ceb1bb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dcecfbd56587ada592c",
+    "url": "/tarea3/static/js/3.1ba47416.chunk.js"
+  },
+  {
+    "revision": "36f158478dda8930dd30",
+    "url": "/tarea3/static/js/main.08fe50f7.chunk.js"
+  },
+  {
+    "revision": "44586a256ffb16695cd7",
+    "url": "/tarea3/static/js/runtime-main.957d1d62.js"
+  }
+]);
